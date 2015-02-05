d6ed4971a643e5adc0dda88b14e8b36a6210db8f
zhengcano <zhengcano@gmail.com>
2015-02-04 16:28:08 -0800

    Completed the Functional-shared pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,47 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+
+
+  extend(newQueue, queueMethods);
+  return newQueue;
 };
 
-var queueMethods = {};
+var extend = function(obj){
+  for (var i = 1; i < arguments.length; i++){
+    for (var key in arguments[i]){
+      obj[key] = arguments[i][key];
+    }
+  }
+  obj.storage = {};
+}
+
+var queueMethods = {
+  count : function(val){
+    for (var key in this.storage){
+      val++;
+      if (arguments[1] === 1 ){
+         this.storage[key - 1] = this.storage[key];
+       }
+    }
+    return val;
+  },
+  enqueue: function(value){
+    var index = this.count(0);
+    this.storage[index] = value;
+  },
+
+  dequeue: function(){
+    var temp = this.storage[0];
+    delete this.storage[0];
+    var index = this.count(0, 1);
+    delete this.storage[index];
+    return temp;
+  },
 
 
-
+  size: function(){
+    return this.count(0);
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.back = 0;
+  newStack.storage = {};
+  extend(newStack, stackMethods);
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+};
 
+stackMethods.push = function (value){
+  this.storage[this.back] = value;
+  this.back++;
+};
+stackMethods.pop = function (){
+  var temp = this.storage[this.back-1];
+  delete this.storage[this.back-1];
+  if (this.back > 0){
+    this.back--;
+  }
+  return temp;
+};
+stackMethods.size = function (){
+  return this.back;
+};
+
+var extend = function (obj){
+  for (var i = 1; i< arguments.length; i++){
+    for (var key in arguments[i]){
+      obj[key]=arguments[i][key];
+    }
+  }
+};

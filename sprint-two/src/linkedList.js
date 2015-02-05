b24be891544133c26067c034d528867c6c277112
zhengcano <zhengcano@gmail.com>
2015-02-05 10:54:47 -0800

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.head === null){
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, node){
+    var isThere = false;
+    var node = node || list.head;
+    if (node.value === target){
+      isThere = true;
+    } else {
+      if (node.next !== null){
+        if (list.contains(target, node.next)){
+          isThere = true;
+        }
+      }
+    }
+    return isThere;
   };
 
   return list;

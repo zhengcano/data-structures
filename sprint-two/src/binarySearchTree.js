104458b994f945c53269c0e7f6445af76c853e05
zhengcano <zhengcano@gmail.com>
2015-02-06 12:06:05 -0800

    Complete binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,11 +1,60 @@
 var BinarySearchTree = function(value){
-  this.value = value;
-  this.left = {};
-  this.right = {};
+  var newTree = Object.create(bsnMethods);
+  newTree.value = value;
+
+  return newTree;
+};
+
+bsnMethods = {};
+
+bsnMethods.insert = function (value){
+  if (value < this.value){
+    if(this.hasOwnProperty('left')){
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  }
+
+  if (value > this.value){
+    if(this.hasOwnProperty('right')){
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+
 
 };
 
-var newBSN = new BinarySearchTree(number);
+bsnMethods.contains = function (value){
+  var isThere = false;
+  if (value === this.value){
+    isThere = true;
+  } else if (value < this.value){
+    if (this.hasOwnProperty('left')){
+      isThere = this.left.contains(value);
+    }
+  }
+  else {
+    if (this.hasOwnProperty('right')){
+      isThere = this.right.contains(value);
+    }
+  }
+  return isThere;
+};
+
+bsnMethods.depthFirstLog = function (cb){
+  cb(this.value);
+  if (this.hasOwnProperty('left')){
+    this.left.depthFirstLog(cb);
+  }
+
+  if (this.hasOwnProperty('right')){
+    this.right.depthFirstLog(cb);
+  }
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
